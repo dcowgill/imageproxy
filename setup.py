17b6756b5a193a47d956dd055bc8cf630acddba0
Daniel Cowgill <dcowgill@gmail.com>
2013-05-06 15:05:23 -0400

    Added dependency on Python>=3.3

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup
+
+REQ_PYTHON = (3,3)
+assert sys.version_info >= REQ_PYTHON, \
+    "python >= {} is required".format(".".join(map(str, REQ_PYTHON)))
 
 setup(name='imageproxy',
       version='1.0',
